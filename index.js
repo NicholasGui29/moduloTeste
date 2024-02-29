4b9bafaadf50012e714fb19da3d75821024d0c08
Nicholas Guilherme <102360510+NicholasGui29@users.noreply.github.com>
2024-02-29 11:59:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+let somadora = (numero1, numero2) => {
+    return numero1 + numero2
+}
+
+
+let substracao = (numero1, numero2) => {
+    return numero1 - numero2
+}
+
+
+
+let multiplicacao = (numero1, numero2) => {
+    return numero1 * numero2
+}
+
+
+let divisao = (numero1, numero2) => {
+    return numero1 / numero2
+}
+
+export default somadora; substracao; multiplicacao; divisao
+
